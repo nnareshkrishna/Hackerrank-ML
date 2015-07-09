578c54017169832ff6c5184ef521af3ae23e5098
nnareshkrishna <nareshkrishnanagarajan@gmail.com>
2015-07-09 23:46:03 +0530

    https://www.hackerrank.com/challenges/document-classification
    Implemented Naive Bayes Classifier after removing stop words. Could not stem since third party libraries were not allowed. The code worked with more than  75% accuracy.The same code exceeded time limit in Stack Exchange question classifier, but results obtained matched with the correct result.

--- naive_bayes.py
+++ naive_bayes.py
@@ -11,8 +11,8 @@ t=int(t2[0])
 #from nltk.corpus import stopwords									#For stopword removal
 #stops=stopwords.words('english')
 stops=['i' , 'me' , 'my' , 'myself' , 'we' , 'our' , 'ours' , 'ourselves' , 'you' , 'your' , 'yours' , 'yourself' , 'yourselves' , 'he' , 'him' , 'his' , 'himself' , 'she' , 'her' , 'hers' , 'herself' , 'it' , 'its' , 'itself' , 'they' , 'them' , 'their' , 'theirs' , 'themselves' , 'what' , 'which' , 'who' , 'whom' , 'this' , 'that' , 'these' , 'those' , 'am' , 'is' , 'are' , 'was' , 'were' , 'be' , 'been' , 'being' , 'have' , 'has' , 'had' , 'having' , 'do' , 'does' , 'did' , 'doing' , 'a' , 'an' , 'the' , 'and' , 'but' , 'if' , 'or' , 'because' , 'as' , 'until' , 'while' , 'of' , 'at' , 'by' , 'for' , 'with' , 'about' , 'against' , 'between' , 'into' , 'through' , 'during' , 'before' , 'after' , 'above' , 'below' , 'to' , 'from' , 'up' , 'down' , 'in' , 'out' , 'on' , 'off' , 'over' , 'under' , 'again' , 'further' , 'then' , 'once' , 'here' , 'there' , 'when' , 'where' , 'why' , 'how' , 'all' , 'any' , 'both' , 'each' , 'few' , 'more' , 'most' , 'other' , 'some' , 'such' , 'no' , 'nor' , 'not' , 'only' , 'own' , 'same' , 'so' , 'than' , 'too' , 'very' , 's' , 't' , 'can' , 'will' , 'just' , 'don' , 'should' , 'now']
-#from nltk.stem.lancaster import LancasterStemmer					#For stemming
-#st=LancasterStemmer()
+from nltk.stem.lancaster import LancasterStemmer					#For stemming
+st=LancasterStemmer()
 
 total=t
 #print t2
